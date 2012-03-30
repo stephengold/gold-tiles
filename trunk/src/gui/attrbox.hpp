0971ef389f6b9462e737794b86d6e2dc958b1988
sgold@sonic.net <sgold@sonic.net@7c49ed60-b72a-f3e7-3011-f5a1496c4a09>
2012-03-30 03:14:56 +0000

    +2 missing files from previous commit

---
+++ trunk/src/gui/attrbox.hpp
@@ -0,0 +1,68 @@
+#ifndef ATTRBOX_HPP_INCLUDED
+#define ATTRBOX_HPP_INCLUDED
+
+// File:    attrbox.hpp
+// Purpose: AttrBox class
+// Author:  Stephen Gold sgold@sonic.net
+// (c) Copyright 2012 Stephen Gold
+// Distributed under the terms of the GNU General Public License
+
+/*
+This file is part of the Gold Tile Game.
+
+The Gold Tile Game is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your 
+option) any later version.
+
+The Gold Tile Game is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with the Gold Tile Game.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+An AttrBox object represents a dialog box for changing how tile attributes
+are displayed in the GUI.
+
+The AttrBox class is an extension of the Dialog class.
+*/
+
+#include "gui/dialog.hpp"       // ISA Dialog
+#include "gui/displaymodes.hpp" // HASA DisplayModes
+#include "tile.hpp"             // USES AIndexType
+
+class AttrBox: public Dialog {
+public:
+    // public lifecycle
+	AttrBox(DisplayModes const &);
+	// no default constructor
+	// ~AttrBox(void);  compiler-generated destructor is OK
+
+	// public operators
+	operator DisplayModes(void) const;
+
+	// misc public methods
+	int HandleMessage(MessageType, Win::WPARAM, Win::LPARAM);
+
+private:
+	// private data
+    DisplayModes mDisplayModes;
+
+	// private lifecycle
+    AttrBox(AttrBox const &);  // not copyable
+
+	// private operators
+    AttrBox &operator=(AttrBox const &); // not assignable
+
+	// misc private methods
+	static AIndexType   AIndex(IdType);
+	void                BumpDisplayMode(AIndexType);
+	static char const * DisplayModeText(ADisplayType);
+	static IdType       ListboxId(AIndexType);
+	void                UpdateValue(IdType, ADisplayType);
+};
+#endif

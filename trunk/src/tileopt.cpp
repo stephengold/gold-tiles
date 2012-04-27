44fa69f667f7fa480fbf0c89df04d8793d7c0716
sgold@sonic.net <sgold@sonic.net@7c49ed60-b72a-f3e7-3011-f5a1496c4a09>
2012-04-27 19:16:16 +0000

    changes and files missed in previous commit

---
+++ trunk/src/tileopt.cpp
@@ -0,0 +1,128 @@
+// File:     tileopt.cpp
+// Location: src
+// Purpose:  implement TileOpt class
+// Author:   Stephen Gold sgold@sonic.net
+// (c) Copyright 2012 Stephen Gold
+// Distributed under the terms of the GNU General Public License
+
+/*
+This file is part of the Gold Tile Game.
+
+The Gold Tile Game is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your 
+option) any later version.
+
+The Gold Tile Game is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with the Gold Tile Game.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "string.hpp"
+#include "tileopt.hpp"
+
+
+// lifecycle
+
+TileOpt::TileOpt(void) {
+	mHasBonus = false;
+}
+
+// new TileOpt from String
+TileOpt::TileOpt(String const& rString) {
+	String copy = rString;
+	copy.Purge();
+
+    if (copy.Last() == BONUS_CHARACTER) {
+        mHasBonus = true;
+		copy.Shorten(1);
+    }
+	mCombo = Combo(copy);
+}
+
+// The compiler-generated copy constructor is fine.
+// The compiler-generated destructor is fine.
+
+
+// operators
+
+// The compiler-generated assignment method is fine.
+
+bool TileOpt::operator==(TileOpt const& rOther) const {
+	bool const result = (mCombo == rOther.mCombo
+		              && mHasBonus == rOther.mHasBonus);
+
+    return result;
+}
+
+TileOpt::operator Combo(void) const {
+    return mCombo;
+}
+
+TileOpt::operator String(void) const {
+	String result = String(mCombo);
+	if (mHasBonus) {
+		result += String(BONUS_CHARACTER);
+	}
+
+	return result;
+}
+
+
+// misc methods
+
+AttrType TileOpt::Attribute(AttrIndexType index) const {
+	AttrType const result = mCombo.Attribute(index);
+
+	return result;
+}
+AttrIndexType TileOpt::CommonAttribute(TileOpt const& rOther) const {
+	AttrIndexType const result = mCombo.CommonAttribute(rOther.mCombo);
+
+	return result;
+}
+AttrCntType TileOpt::CountMatchingAttributes(TileOpt const& rOther) const {
+	AttrCntType const result = mCombo.CountMatchingAttributes(rOther.mCombo);
+
+	return result;
+}
+void TileOpt::SetAttribute(AttrIndexType index, AttrType value) {
+    mCombo.SetAttribute(index, value);
+}
+
+void TileOpt::SetBonus(bool newBonus) {
+	mHasBonus = newBonus;
+}
+
+
+// inquiry methods
+
+bool TileOpt::HasAttribute(AttrIndexType index, AttrType value) const {
+	bool const result = mCombo.HasAttribute(index, value);
+
+	return result;
+}
+
+bool TileOpt::HasBonus(void) const {
+    return mHasBonus;
+}
+
+bool TileOpt::IsCompatibleWith(TileOpt const& rOther) const {
+	bool const result = mCombo.IsCompatibleWith(rOther);
+
+	return result;
+}
+
+bool TileOpt::MatchesString(String const& rMatch) const {
+	String string_form = String(*this);
+	string_form = string_form.Purge();
+	String const match = rMatch.Purge();
+
+	bool const result = (string_form == match);
+
+	return result;
+}
---
+++ trunk/src/tileopt.hpp
@@ -0,0 +1,73 @@
+#ifndef TILEOPT_HPP_INCLUDED
+#define TILEOPT_HPP_INCLUDED
+
+// File:     tileopt.hpp
+// Location: src
+// Purpose:  declare TileOpt class
+// Author:   Stephen Gold sgold@sonic.net
+// (c) Copyright 2012 Stephen Gold
+// Distributed under the terms of theGNU General Public License
+
+/*
+This file is part of the Gold Tile Game.
+
+The Gold Tile Game is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your 
+option) any later version.
+
+The Gold Tile Game is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with the Gold Tile Game.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+A TileOpt object represents the player-visible options of a Tile, namely
+its bonus value and attributes.
+
+The TileOpt class is implemented by combining a bool (for the bonus)
+with Combo (for the attributes).
+*/
+
+#include "combo.hpp"    // HASA Combo
+
+class TileOpt {
+public:
+    // public lifecycle
+	TileOpt(void);
+	explicit TileOpt(String const&);
+	// TileOpt(TileOpt const&);  compiler-generated copy constructor is OK
+	// ~TileOpt(void);  compiler-generated destructor is OK
+
+    // public operators
+	// TileOpt& operator=(TileOpt const&);  compiler-generated assignment operator is OK
+	bool     operator==(TileOpt const&) const;
+	operator Combo(void) const;
+	operator String(void) const;
+
+	// misc public methods
+	AttrType      Attribute(AttrIndexType) const;
+	AttrIndexType CommonAttribute(TileOpt const&) const;
+	AttrCntType   CountMatchingAttributes(TileOpt const&) const;
+	void          SetAttribute(AttrIndexType, AttrType);
+	void          SetBonus(bool);
+
+	// public inquiry methods
+	bool HasAttribute(AttrIndexType, AttrType) const;
+	bool HasBonus(void) const;
+	bool IsCompatibleWith(TileOpt const&) const;
+	bool MatchesString(String const&) const;
+
+private:
+	// private constants
+	static const char BONUS_CHARACTER = '+';
+
+	// private data
+	Combo mCombo;    // all attributes
+	bool  mHasBonus;
+};
+#endif // !defined(TILEOPT_HPP_INCLUDED)
